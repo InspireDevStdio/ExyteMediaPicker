f04eedc9a4105b212f650faa51bc8f653fc4fb67
FireLord <firelord.xda@gmail.com>
2026-02-02 12:55:09 +0530

    Use 1.0.1 of ExyteAnchoredPopup

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/InspireDevStdio/ExyteAnchoredPopup.git",
-            from: "1.0.0"
+            from: "1.0.1"
         )
     ],
     targets: [
